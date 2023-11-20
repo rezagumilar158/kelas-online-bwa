14cacfeb4b9d8536eabbba43315cf55fdf579d2c
rezagumilar158 <rezagumilar158@gmail.com>
2023-11-20 21:28:23 +0700

    update handler users index

--- service-user/routes/handler/users/index.js
+++ service-user/routes/handler/users/index.js
@@ -1,6 +1,9 @@
 const register = require('./register');
 const login = require('./login');
 const update = require('./update');
+const getUser = require('./getUser');
+const getUsers = require('./getUsers');
+const logout = require('./logout');
 
 
 
@@ -8,6 +11,8 @@ const update = require('./update');
 module.exports = {
   register,
   login,
-  update
-
+  update,
+  getUser,
+  getUsers,
+  logout
 }
